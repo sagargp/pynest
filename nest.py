4b645431f68969852ea57c5e9bfbb31624024c93
Sagar Pandya <sagargp@gmail.com>
2014-11-02 23:32:01 -0700

    Takes username/pass from the CLI. Also, added an option for CSV output

--- nest.py
+++ nest.py
@@ -1,10 +1,11 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
-
 import urllib
 import urllib2
 import sys
 import json
+import getpass
+import datetime
 from optparse import OptionParser
 
 class Nest:
@@ -88,13 +89,15 @@ if __name__ == "__main__":
   parser.add_option("-t", "--temperature", action="store_true", help="print the temperature")
   parser.add_option("-f", "--fahrenheit", action="store_true", help="temp should be in fahrenheit (default is celsius)")
   parser.add_option("-a", "--all", action="store_true", help="print both temp and humidity")
+  parser.add_option("-c", "--csv", action="store_true", help="print both temp and humidity in CSV format for easy appending to a log file")
   opts, args = parser.parse_args()
 
-  if not opts.user or not opts.password:
-    print "Username and password are required"
-    sys.exit(-1)
+  _user = opts.user
+  _pass = opts.password
+  if not _user: _user = raw_input("Username: ")
+  if not _pass: _pass = getpass.getpass()
 
-  n = Nest(opts.user, opts.password, opts.serial, opts.index)
+  n = Nest(_user, _pass, opts.serial, opts.index)
   n.login()
   n.update()
 
@@ -104,8 +107,13 @@ if __name__ == "__main__":
     if opts.fahrenheit:
       t = 9*t/5 + 32
       suffix = "F"
-    print "%0.1f° %s" % (t, suffix),
-    print "@ %d%% humidity" % n.get_humidity()
+    if opts.csv:
+      now = datetime.datetime.now()
+      time = "%02d:%02d" % (now.hour, now.minute)
+      print "%s,%0.1f,%d" % (time, t, n.get_humidity())
+    else:
+      print "%0.1f° %s" % (t, suffix),
+      print "@ %d%% humidity" % n.get_humidity()
   else:
     if opts.temperature:
       t = n.get_temp()
