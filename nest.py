6a7c934376bd26ddf7f4ecc7748226599ac61dbc
Sagar Pandya <sagargp@gmail.com>
2014-12-07 21:20:41 -0700

    Added a hack to workaround a python SSL bug

--- nest.py
+++ nest.py
@@ -1,5 +1,18 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+
+import ssl
+from functools import wraps
+
+def sslwrap(func):
+    @wraps(func)
+    def bar(*args, **kw):
+      kw['ssl_version'] = ssl.PROTOCOL_TLSv1
+      return func(*args, **kw)
+    return bar
+
+ssl.wrap_socket = sslwrap(ssl.wrap_socket)
+
 import urllib
 import urllib2
 import sys
